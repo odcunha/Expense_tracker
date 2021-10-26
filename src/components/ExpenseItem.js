c715302b5d5fec303ba220a795b0a62cab9b5209
odcunha <odcunha@deakin.edu.au>
2021-10-26 20:25:59 +1100

    Expenses component added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,11 @@
 import "./ExpenseItem.css";
-
+import ExpenseDate from "./ExpenseDate";
+import { tsPropertySignature } from "@babel/types";
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
-
   return (
     <>
       <div className="expense-item">
-        <div className="expense-item__date">
-          <div>{month}</div>
-          <div>{day}</div>
-          <div>{year}</div>
-        </div>
+        <ExpenseDate date={props.date} />
         <div className="expense-item__description">
           <h2>{props.title}</h2>
           <div className="expense-item__price">${props.amount}</div>
